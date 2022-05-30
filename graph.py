1400d4f5498577c07a73f399392cc4f8b44c4556
nggill <92691516+nggill@users.noreply.github.com>
2022-05-30 17:34:59 -0500

    created vertex values and mapping for generator

--- graph.py
+++ graph.py
@@ -17,3 +17,49 @@ import random
 class Vertex:
     def __init__(self, value): # value will be the word
         self.value=value
+        self.adjacent = {} # nodes that have an edge from this vertex
+        self.neighbors = []
+        self.neighbors_weights = []
+
+
+
+    def add_edge_to(self, vertex, weight=0):
+        self.adjacnet[vertex] = weight
+    
+    def increment_edge(self, vertex):
+        # this is incrementing the weight of the edge
+        self.adjacent[vertex] = self.adjacent.get(vertex, 0) + 1
+
+    def get_probability_map(self):
+        for (vertex, weight) in self.adjacent.items():
+            self.neighbors.append(vertex)
+            self.neighbors_weights.append(weight)
+
+    def next_word(self):
+        # randomly select next word based on weights
+        return random.choices(self.neighbors, weight=self.neighbors_weights)[0]
+
+class Graph:
+    def __init__(self):
+        self.vertices = {}
+
+    def get_vertex_values(self):
+        # what are the values of all the vertices?
+        # in other words, return all possible words
+        return set(self.vertices.keys())
+
+    def add_vertex(self, value):
+        self.vertices[value] = Vertex(value)
+
+    def get_vertex(self, value):
+        # what if the value isnt in the graph?
+        if value not in self.vertices:
+            self.add_vertex(value)
+        return self.verticles[value]
+
+    def get_next_word(self, current_vertex):
+        self.verticles[current_vertex.value]
+
+    def generate_probability_mappings(self):
+        for vertex in self.vertices.values():
+            vertex.get_probability_map()
